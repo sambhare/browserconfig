8ae9038feea743b9e40647333cb86e47c15f4ad5
Rajas Sambhare <sambhare@users.noreply.github.com>
2023-02-09 18:54:53 -0500

    Replaced md with html

--- bookmarklets.js
+++ bookmarklets.js
@@ -1,11 +1,11 @@
 function zapFonts() {
     const newFont = prompt('Font','Open Sans');
-    const xzCss = '@import url(`https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/' + newFont.toLocaleLowerCase().replace(' ','-') + '.min.css`);';
-    const gFontsCss = '@import url(`https://fonts.googleapis.com/css?family=' + newFont.replace(' ','+') + '&display=swap`);';
+    const xzCss = '@import url("https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/' + newFont.toLocaleLowerCase().replace(' ','-') + '.min.css");';
+    const gFontsCss = '@import url("https://fonts.googleapis.com/css?family=' + newFont.replace(' ','+') + '&display=swap");';
     const newCss = xzCss + gFontsCss + '*{font-family:' + newFont +'!important}code,pre,tt{font-family:monospace!important;}';
     const a = document.createElement('link');
     a.rel = 'stylesheet';
     a.href = 'data:text/css,' + encodeURI(newCss);
     document.getElementsByTagName('head')[0].appendChild(a);
 }
-javascript:(function zapFonts(){const newFont=prompt('Font','Open Sans');const xzCss='@import url(`https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/'+newFont.toLocaleLowerCase().replace(' ','-')+'.min.css`);';const gFontsCss='@import url(`https://fonts.googleapis.com/css?family='+newFont.replace(' ','+')+'&display=swap`);';const newCss=xzCss+gFontsCss+'*{font-family:'+newFont+'!important}code,pre,tt{font-family:monospace!important;}';const a=document.createElement('link');a.rel='stylesheet';a.href='data:text/css,'+encodeURI(newCss);document.getElementsByTagName('head')[0].appendChild(a);})();
+javascript:(function zapFonts(){const newFont=prompt('Font','Open Sans');const xzCss='@import url(&quot;https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/'+newFont.toLocaleLowerCase().replace(' ','-')+'.min.css&quot;);';const gFontsCss='@import url(&quot;https://fonts.googleapis.com/css?family='+newFont.replace(' ','+')+'&display=swap&quot;);';const newCss=xzCss+gFontsCss+'*{font-family:'+newFont+'!important}code,pre,tt{font-family:monospace!important;}';const a=document.createElement('link');a.rel='stylesheet';a.href='data:text/css,'+encodeURI(newCss);document.getElementsByTagName('head')[0].appendChild(a);})();
