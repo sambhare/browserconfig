214aed37aa5e4c36cdd6848fc76693d2947c41d9
Rajas Sambhare <sambhare@users.noreply.github.com>
2023-02-21 09:44:29 -0500

    Trim font name

--- bookmarklets.js
+++ bookmarklets.js
@@ -1,5 +1,5 @@
 function zapFonts() {
-    const newFont = prompt('Font','Open Sans');
+    const newFont = prompt('Font','Open Sans').trim();
     const xzCss = '@import url("https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/' + newFont.toLocaleLowerCase().replace(' ','-') + '.min.css");';
     const gFontsCss = '@import url("https://fonts.googleapis.com/css?family=' + newFont.replace(' ','+') + '&display=swap");';
     const newCss = xzCss + gFontsCss + '*{font-family:' + newFont +'!important}code,pre,tt{font-family:monospace!important;}';
@@ -8,4 +8,4 @@ function zapFonts() {
     a.href = 'data:text/css,' + encodeURI(newCss);
     document.getElementsByTagName('head')[0].appendChild(a);
 }
-javascript:(function zapFonts(){const newFont=prompt('Font','Open Sans');const xzCss='@import url(&quot;https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/'+newFont.toLocaleLowerCase().replace(' ','-')+'.min.css&quot;);';const gFontsCss='@import url(&quot;https://fonts.googleapis.com/css?family='+newFont.replace(' ','+')+'&display=swap&quot;);';const newCss=xzCss+gFontsCss+'*{font-family:'+newFont+'!important}code,pre,tt{font-family:monospace!important;}';const a=document.createElement('link');a.rel='stylesheet';a.href='data:text/css,'+encodeURI(newCss);document.getElementsByTagName('head')[0].appendChild(a);})();
+javascript:(function zapFonts(){const newFont=prompt('Font','Open Sans').trim();const xzCss='@import url(&quot;https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/'+newFont.toLocaleLowerCase().replace(' ','-')+'.min.css&quot;);';const gFontsCss='@import url(&quot;https://fonts.googleapis.com/css?family='+newFont.replace(' ','+')+'&display=swap&quot;);';const newCss=xzCss+gFontsCss+'*{font-family:'+newFont+'!important}code,pre,tt{font-family:monospace!important;}';const a=document.createElement('link');a.rel='stylesheet';a.href='data:text/css,'+encodeURI(newCss);document.getElementsByTagName('head')[0].appendChild(a);})();
